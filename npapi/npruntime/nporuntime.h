48033d2feff802c6f632584f6904f4bb6767b9b4
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-03-27 13:29:45 +0100

    npappi: nporuntime: Remove declaration of never implemented methods

--- npapi/npruntime/nporuntime.h
+++ npapi/npruntime/nporuntime.h
@@ -101,12 +101,6 @@ protected:
 
     friend void RuntimeNPClassDeallocate(NPObject *npobj);
     friend void RuntimeNPClassInvalidate(NPObject *npobj);
-    template <class RuntimeNPObject> static bool RuntimeNPClassGetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
-    template <class RuntimeNPObject> static bool RuntimeNPClassSetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
-    template <class RuntimeNPObject> static bool RuntimeNPClassRemoveProperty(NPObject *npobj, NPIdentifier name);
-    template <class RuntimeNPObject> static bool RuntimeNPClassInvoke(NPObject *npobj, NPIdentifier name,
-                                                    const NPVariant *args, uint32_t argCount,
-                                                    NPVariant *result);
     friend bool RuntimeNPClassInvokeDefault(NPObject *npobj,
                                             const NPVariant *args,
                                             uint32_t argCount,
